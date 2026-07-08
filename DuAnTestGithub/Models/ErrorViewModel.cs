c06b25c4097c079fc32034abd1635e3e737f7117
Thành Lê <thanhle36668@gmail.com>
2026-07-08 13:07:53 +0700

    Init Project

---
+++ DuAnTestGithub/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DuAnTestGithub.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
